8d5e8978b07b20d6f67fabd5de18e5b2d09e41ac
888Greys <toxicgreys001@gmail.com>
2024-09-06 14:17:05 +0300

    Refactor DBClient class and update MongoDB utility functions

--- utils/db.js
+++ utils/db.js
@@ -14,27 +14,43 @@ After the class definition, create and export an instance of DBClient called dbC
 */
 
 import { MongoClient } from 'mongodb';
+// import { MongoClient } from 'mongodb/lib/mongo_client';
 
-class DBClient { 
+class DBClient {
+  // A class with basic connection to a mongodb server
   constructor() {
-    this.host = process.env.DB_HOST || 'localhost';
-    this.port = process.env.DB_PORT || 27017;
-    this.database = process.env.DB_DATABASE || 'files_manager';
-    this.client = new MongoClient(`mongodb://${this.host}:${this.port}`, { useUnifiedTopology: true });
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+
+    const uri = `mongodb://${host}:${port}/${database}`;
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    this.client = client;
     this.client.connect();
-    this.db = this.client.db(this.database);
+    this.db = client.db(database);
   }
 
+  // checks if the mongoddb is connected
   isAlive() {
-    return this.client.isConnected();
+    return this.client.topology.isConnected();
   }
 
   async nbUsers() {
-    return this.db.collection('users').countDocuments();
+    try {
+      const users = await this.db.collection('users').countDocuments();
+      return users;
+    } catch (err) {
+      throw new Error(`Unable to get number of users ${err.message}`);
+    }
   }
 
   async nbFiles() {
-    return this.db.collection('files').countDocuments();
+    try {
+      const files = await this.db.collection('files').countDocuments();
+      return files;
+    } catch (err) {
+      throw new Error(`Unable to get number of files ${err.message}`);
+    }
   }
 }
 
